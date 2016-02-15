f8a2c6a6553340eca4f7a5d718779d3524ea5726
Clément <clement.guerineau@creatediy.fr>
2016-02-15 11:19:24 +0100

    ajout d'un fichier head inclue sur chaque page.

--- demande.php
+++ demande.php
@@ -22,14 +22,7 @@
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
-  <head>
-  <meta http-equiv="content-type" content="text/html; charset=utf-8">
-  <title>Monnaie M - Expérimentation d'une monnaie complémentaire assortie d'un revenu de base</title>
-  <link rel="stylesheet" href="monnaiem.css" typeproduit="text/css">
-  <meta name="description" content="Monnaie M est une expérimentation visant à faire connaître et promouvoir le fonctionnement et le rôle d'une monnaie, 
-  les Systèmes d'Echanges Locaux, le concept de revenu de base, les monnaies complémentaires.">
-  <meta name="keywords" lang="fr" content="monnaie bitcoin openudc création monétaire SEL revenu de base dividende universel">
-  </head>
+  <?php include("head.php"); ?>
   <body>
 <?php 
 
